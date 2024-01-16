110fa4851b336308be6346ad50f65cd301b37dfa
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-01-16 03:22:07 -0300

    user post 2

--- endpoints/user_post.php
+++ endpoints/user_post.php
@@ -1,10 +1,31 @@
 <?php 
 
 function apiUserPost($request) {
-  $response = [
-    'ID' => '2',
-    'user_login' => 'teste',
-  ];
+  $username = sanitize_text_field($request['username']);
+  $password = $request['password'];
+  $email = sanitize_email($request['email']);
+
+  if (empty($username) || empty($password) || empty($email)) {
+    $response = new WP_Error('error', 'Dados incompletos', ['status' => 406]);
+    return rest_ensure_response($response);
+  }
+
+  if (username_exists($username)) {
+    $response = new WP_Error('error', 'Nome de usuário já existe', ['status' => 403]);
+    return rest_ensure_response($response);
+  }
+
+  if (email_exists($email)) {
+    $response = new WP_Error('error', 'Email já cadastrado', ['status' => 403]);
+    return rest_ensure_response($response);
+  }
+
+  $response = wp_insert_user([
+    'user_login' => $username,
+    'user_pass' => $password,
+    'user_email' => $email,
+    'role' => 'subscriber',
+  ]);
   return rest_ensure_response($response);
 }
 
